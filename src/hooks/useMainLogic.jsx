dcc46b67483a23a87c2f749a8254aeab40edf6b4
erdemir123 <sadikerdemir93@gmail.com>
2022-11-06 16:25:18 +0300

    feat:movieDetails is designed

--- src/hooks/useMainLogic.jsx
+++ src/hooks/useMainLogic.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { api_key, api_url, base_url } from "../constants/API";
 import { UseLoginContext } from "../context/LoginProvider";
-import Swal from 'sweetalert2'
+
 
 
 
--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,15 +1,42 @@
-import React from 'react'
-import { useLocation } from 'react-router-dom'
+import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 const MovieDetail = () => {
-    const {state} = useLocation()
-    console.log(state.id)
+  const { state } = useLocation();
+  const navigate = useNavigate();
+  const image_url = `https://image.tmdb.org/t/p/w500`;
+  console.log(state);
   return (
-    
-    <div>
-        sadık
-        
+    <div className="flex justify-center mt-20 ">
+      <div className="items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-3xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+        <img
+          className="w-[300px] rounded-t-lg mx-auto md:rounded-none md:rounded-l-lg mt-4 "
+          src={image_url + state?.backdrop_path}
+          alt=""
+        />
+        <div className="flex flex-col justify-between p-4 leading-normal">
+          <h5 className="my-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">
+            {state.title}
+          </h5>
+          <p className="mb-3 font-bold text-lg text-gray-700 dark:text-gray-400 text-center">
+            {state.overview}
+          </p>
+          <div className="flex justify-between my-5 mx-4">
+            <p className="font-bold text-lg">{state.release_date}</p>
+            <p
+              className={`font-bold text-lg  text-white rounded-md ${
+                state.vote_average <= 7
+                  ? "bg-red-500 px-2 py-1 "
+                  : "bg-orange-400 px-2 py-1 "
+              }`}
+            >
+              {state.vote_average}
+            </p>
+          </div>
+          <button onClick={() => navigate("/")} className="bg-slate-500 w-28 mx-auto py-2 px-2 font-bold text-lg  text-white rounded-md">Go Home</button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MovieDetail
+export default MovieDetail;
